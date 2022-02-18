06ed091c36f6a17980bd63c33720dab872b7bfa8
Yuzhou Mao <myz@umich.edu>
2022-02-18 14:27:22 -0500

    add: unit test setup

---
+++ test/test_container.py
@@ -0,0 +1,14 @@
+import unittest
+
+from elastic.container import DataContainer
+
+class TestDataContainer(unittest.TestCase):
+    def test_init(self):
+        int1 = 1
+        d1 = DataContainer(int1, prevOpEvent=None)
+        
+        self.assertEqual(d1.get_base_type(), int)
+        self.assertEqual(d1.get_base_id(), id(int1))
+
+if __name__ == '__main__':
+    unittest.main()

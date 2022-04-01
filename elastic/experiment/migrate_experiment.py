ecce840069f3b40ec06a85e367e3df1115c15dc1
Yuzhou Mao <yuzhom@umich.edu>
2022-04-01 16:29:20 -0400

    fix: recovering the migrated variables requires a global context

--- elastic/experiment/migrate_experiment.py
+++ elastic/experiment/migrate_experiment.py
@@ -6,6 +6,7 @@
 from core.io.external_storage import ExternalStorage
 from core.io.filesystem_adapter import FilesystemAdapter
 from experiment.migrate import migrate
+from experiment.recover import resume
 
 import torch 
 import torchvision
@@ -13,16 +14,30 @@ import os
 
 
 def main():
-    a = 100
-    resnet = torchvision.models.resnet18(pretrained=True)
+    a = [[1], [2], [3]]
+    b = [a[0], a[1]] # fundamental limitation? when a and b are restored, they will no longer be sharing items
+
+    # (a, b)
+    # is pickle able to preserve the underlying object id when pickling `c`?
+    # suppose pickle has the ability to magically figure out the dependencies when restoring 
+    #
+    c = (a, b)
     
+    # [resnet, net]
+
+    resnet = torchvision.models.resnet18(pretrained=True)
+    net = resnet
+    nn = [resnet, [net]]
+
     storage = ExternalStorage()
     storage.register("local", FilesystemAdapter())
     
     glbs, locs = globals(), locals()
     context_items = {**glbs, **locs}.items()
 
-    migrate([a, resnet], "", storage, context_items)
+    # TODO: modify migrate and recover to wrap all migrated objects in a single list
+    migrate([a, nn], "", storage, context_items)
+    resume(storage)
 
 
 if __name__ == '__main__':
--- elastic/experiment/recover.py
+++ elastic/experiment/recover.py
@@ -4,14 +4,17 @@
 # Copyright 2021-2022 University of Illinois
 
 import json
+from pathlib import Path
 import pickle
+from typing import Dict
 
 from core.common.migration_metadata import MigrationMetadata
 from core.io.external_storage import ExternalStorage
 
 from experiment.migrate import METADATA_PATH
 
-def resume(storage: ExternalStorage):
+def resume(storage: ExternalStorage,
+           global_context: Dict):
     """
     (1) Busy waits for the file at `migration_metadata_path` in `storage` to appear
     (2) Once the metadata file appears, read the metadata content
@@ -26,17 +29,17 @@ def resume(storage: ExternalStorage):
         storage (ExternalStorage):
             a wrapper for any storage adapter (local fs, cloud storage, etc.)
     """
-    metadata = json.loads(storage.read_all(METADATA_PATH))
+    metadata = json.loads(storage.read_all(Path(METADATA_PATH)))
     metadata = MigrationMetadata.from_json(metadata)
     
     # run the recomputation code to get the state of objects that are not migrated
     # this run can be done using a single cell of the new Python session at destination
     # NOTE: this is only needed when we finish experiments and focus on engineering
 
-    for obj_path, obj_names in metadata.get_objects_migrated():
-        object_pickled = storage.read_all(obj_path)
+    for obj_path, obj_names in metadata.get_objects_migrated().items():
+        object_pickled = storage.read_all(Path(obj_path))
         obj = pickle.loads(object_pickled)
         
         # link the reconstructed object to the list of object names
         for name in obj_names:
-            globals()[name] = obj
+            global_context[name] = obj

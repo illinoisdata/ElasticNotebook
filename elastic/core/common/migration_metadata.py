251669fbf8012f656444f894ffed6e004e06bef5
Hui Yu <hyu.alina@gmail.com>
2022-09-12 06:28:39 -0500

    upgrade migrate & recover
    merge updated recover and migrate

--- elastic/core/common/migration_metadata.py
+++ elastic/core/common/migration_metadata.py
@@ -4,16 +4,20 @@
 # Copyright 2021-2022 University of Illinois
 
 import json
+from typing import List, Dict
 
 KEY_OBJECTS_MIGRATED = "objectsMigrated"
 KEY_RECOMPUTE_CODE = "recomputeCode"
+KEY_INPUT_MAPPINGS = "inputMappings"
+KEY_OUTPUT_MAPPINGS = "outputMappings"
+KEY_ORDER_LIST = "orderList"
 
 class MigrationMetadata:
     def __init__(self):
         pass
 
 
-    def with_objects_migrated(self, objects: dict):
+    def with_objects_migrated(self, objects: Dict):
         self.objects_migrated = objects
         return self
 
@@ -22,7 +26,7 @@ class MigrationMetadata:
         return self.objects_migrated
 
 
-    def with_recompute_code(self, recompute: list):
+    def with_recompute_code(self, recompute: Dict):
         self.recomputeCode = recompute
         return self
 
@@ -31,14 +35,47 @@ class MigrationMetadata:
         return self.recomputeCode
 
 
+    def with_input_mappings(self, input: Dict):
+        self.input_mappings = input
+        return self
+
+
+    def get_input_mappings(self):
+        return self.input_mappings
+
+
+    def with_output_mappings(self, ouput: Dict):
+        self.output_mappings = ouput
+        return self
+
+
+    def get_output_mappings(self):
+        return self.output_mappings
+
+
+    def with_order_list(self, order: List):
+        self.order_list = order
+        return self
+
+
+    def get_order_list(self):
+        return self.order_list
+
+
     def to_json_str(self) -> str:
         return json.dumps({
             "objectsMigrated": self.objects_migrated,
-            "recomputeCode": self.recomputeCode
+            "recomputeCode": self.recomputeCode,
+            "inputMappings": self.input_mappings,
+            "outputMappings": self.output_mappings,
+            "orderList": self.order_list
         })
 
 
     @staticmethod
-    def from_json(kv: dict):
+    def from_json(kv: Dict):
         return MigrationMetadata().with_objects_migrated(kv[KEY_OBJECTS_MIGRATED])\
-                                  .with_recompute_code(KEY_RECOMPUTE_CODE)
+                                  .with_recompute_code(kv[KEY_RECOMPUTE_CODE])\
+                                  .with_input_mappings(kv[KEY_INPUT_MAPPINGS])\
+                                  .with_output_mappings(kv[KEY_OUTPUT_MAPPINGS])\
+                                  .with_order_list(kv[KEY_ORDER_LIST])
--- elastic/core/io/recover.py
+++ elastic/core/io/recover.py
@@ -6,31 +6,67 @@
 import json
 from pathlib import Path
 import pickle
-from typing import Dict
+from typing import Dict, List
 
 from core.common.migration_metadata import MigrationMetadata
 from core.io.external_storage import ExternalStorage
 
 from core.io.migrate import METADATA_PATH
 
-def resume(storage):
+def setDictValues(d: Dict, keySet: List, valueSet: List):
+    for k, v in zip(keySet, valueSet):
+        d[k] = v
+
+def wrapperFunc(func, args):
+    return func(*args)
+
+def resume(storage: ExternalStorage,
+           global_state: Dict):
     """
     (1) Busy waits for the file at `migration_metadata_path` in `storage` to appear
     (2) Once the metadata file appears, read the metadata content
-        (2a) Recover a list of objects according to the metadata, which should contain
-             a list of pairs <path, object_name>.
-        (2b) For each object in the list, unpickle the corresponding file in storage
+        (2a) Recover a list of objects / operation events according to the metadata, 
+             which should contain a list of pairs <path, name>.
+        (2b) For each object / oe in the list, unpickle the corresponding file in storage
         (2c) Start recomputation (if needed) as instructed in the metadata.
 
     Args:
-        migration_metadata_path (MigrationMetadata):
-            path to a JSON file in `storage` containing migration metadata
         storage (ExternalStorage):
             a wrapper for any storage adapter (local fs, cloud storage, etc.)
+        global_state (Dict):
+            a dictionary to store recovered states (can just pass in globals())
     """
-    file = open(storage, 'rb')
-    items = pickle.load(file)
-    data_container_dict = items[0]
-    recomputation_code = items[1]
-    
-    return data_container_dict, recomputation_code
+    # file = open(storage, 'rb')
+    # items = pickle.load(file)
+    # data_container_dict = items[0]
+    # recomputation_code = items[1]
+    #
+    # return data_container_dict, recomputation_code
+    # run the recomputation code to get the state of objects that are not migrated
+    # this run can be done using a single cell of the new Python session at destination
+    # NOTE: this is only needed when we finish experiments and focus on engineering
+
+    for obj_path, obj_name in metadata.get_objects_migrated().items():
+        object_pickled = storage.read_all(Path(obj_path))
+        obj = pickle.loads(object_pickled)
+        global_state[obj_name] = obj
+        globals()[obj_name] = obj
+    for oe_path, oe_name in metadata.get_recompute_code().items():
+        oe_pickled = storage.read_all(Path(oe_path))
+        oe = pickle.loads(oe_pickled)
+        global_state[oe_name] = oe
+        globals()[oe_name] = oe
+
+    # recompute based on order list and input/output mappings
+    order = metadata.get_order_list()
+    input_mappings = metadata.get_input_mappings()
+    output_mappings = metadata.get_output_mappings()
+    for item in order:
+        input_variables = []
+        if item in input_mappings:
+            input_names = input_mappings[item]
+            input_variables = [globals()[k] for k in input_names]
+        output_variables = output_mappings[item]
+        output_values = wrapperFunc(globals()[item], input_variables)
+        setDictValues(global_state, output_variables, output_values)
+        setDictValues(globals(), output_variables, output_values)
